0160f45db1ac0f42bf9abd2b60651b5116dc23df
J Lyn <nesperos5@gmail.com>
2023-04-11 15:56:51 +0800

    minor adjustment
    1. Change quantization level from 8 to 16.

--- huffmanAudio.m
+++ huffmanAudio.m
@@ -3,11 +3,11 @@ close all; clear; clc;
 %% Read the audio file
 [sig, Fs] = audioread('swamp.mp3');
 
-%% Extract the audio signal from the first channel
+%% Extract the audio signal from the left channel
 sigl = sig(:,1);
 
 %% Quantize the audio signal
-sig_quantized = round(sigl*(2^8-1))/(2^8-1);
+sig_quantized = round(sigl*(2^16-1))/(2^16-1);
 
 %% Calculate the probability of occurrence of each symbol
 symbols = unique(sig_quantized);
@@ -22,5 +22,5 @@ encoded_audio = huffmanenco(sig_quantized, dict);
 
 %% Save the compressed audio as a WAV file
 audiowrite('swamp_compressed.mp3', encoded_audio, Fs);
-dequantized_audio = encoded_audio * (2^8-1);
-audiowrite('swamp_deq.mp3', dequantized_audio, Fs);
+% dequantized_audio = encoded_audio * (2^8-1);
+% audiowrite('swamp_deq.mp3', dequantized_audio, Fs);
